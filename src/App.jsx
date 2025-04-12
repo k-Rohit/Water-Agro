aa99e53c0650392d93497720498d6d8df70a1699
Yashasvi222 <yashasviagrawal7@gmail.com>
2025-04-12 15:18:23 +0530

    Landing Page added (developing)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import ContactUs from './pages/ContactUs';
+import LandingPage from './pages/LandingPage';
 
 function App() {
 
   return (
     <>
-      <ContactUs/>
+      <LandingPage/>
     </>
   )
 }
