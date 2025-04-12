f5422595a5eadf927c5f8cd6659465aa4807444c
MedhanshPatley <medhansh.patley.btech2021@sitpune.edu.in>
2025-04-12 15:22:48 +0530

    added slideshow component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import ContactUs from './pages/ContactUs';
-
-// function App() {
-
-//   return (
-//     <>
-//       <ContactUs/>
-//     </>
-//   )
-// }
-
-// export default App
-
-import React from 'react';
-import Slideshow from './components/Slideshow';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ContactUs from './pages/ContactUs';
 
 function App() {
+
   return (
-    <div>
-      <Slideshow />
-    </div>
-  );
+    <>
+      <ContactUs/>
+    </>
+  )
 }
 
-export default App;
+export default App
+
+// import React from 'react';
+// import Slideshow from './components/Slideshow';
+
+// function App() {
+//   return (
+//     <div>
+//       <Slideshow />
+//     </div>
+//   );
+// }
+
+// export default App;
