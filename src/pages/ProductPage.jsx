94a68cd222ca34bc0d69d2f2535054b779e42688
Kumar Rohit <91978766+kumarrohit2509@users.noreply.github.com>
2025-04-13 10:09:04 +0530

    productpage added

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 export const ProductPage = () => {
   return (
-    <div>ProductPage</div>
+    <>
+    <div className='navbar'>
+        <Navbar/>
+    </div>
+    <div className='product-page-container'>
+        <div className='cateogry-icons'>
+            
+        </div>
+    </div>
+
+    </>
   )
 }
