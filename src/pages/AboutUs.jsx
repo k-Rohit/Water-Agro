007915fd17c4282b9d758e9c8e71ecb46d614719
Kumar Rohit <91978766+kumarrohit2509@users.noreply.github.com>
2025-04-20 12:04:40 +0530

    changing aboutus

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -25,7 +25,7 @@ export const AboutUs = () => {
                     <div className='aboutus-heading'>
                         <span className='who'>Who</span> we are
                     </div>
-                    <div className='ourteam-text'>
+                    <div className='aboutus-text'>
                         At Water Agro Life LLP, we believe that the strength of a nation begins in its fields. Our journey is driven by a simple yet powerful goal — to deliver practical, high-quality agricultural solutions that make farming more productive, sustainable, and rewarding. Founded in 2024 by Mr. Rajat Kumar, the company was established a vision to bridge the gap between innovative crop care and the real needs of Indian farmers. We work hand-in-hand with growers, understanding their challenges, and providing products that support them through every stage of the crop cycle.
                     </div>
                 </div>
