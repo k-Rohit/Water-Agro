57c35b93189fc7c7295506b1e9a7e10ad10467b5
Kumar Rohit <91978766+kumarrohit2509@users.noreply.github.com>
2025-04-21 22:28:19 +0530

    nav link order change

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -72,10 +72,10 @@ const LandingPage = () => {
           <div className="div-right">
             <div className="tabs">
               <Link to="/" className="tabs-home tab-button">Home</Link>
-              <Link to="/products" className="tabs-products tab-button">Products</Link>
               <Link to="/about-us" className="tabs-aboutus tab-button">About Us</Link>
+              <Link to="/products" className="tabs-products tab-button">Products</Link>
+              <Link to="/ourcorp" className="tabs-corp tab-button"> Our Corp Family</Link>
               <Link to="/research" className="tabs-ri tab-button">R&I</Link>
-              <Link to="/ourcorp" className="tabs-corp tab-button">Corp Family</Link>
               <Link to="/contact-us" className="tabs-contactus tab-button">Contact Us</Link>
               
 
